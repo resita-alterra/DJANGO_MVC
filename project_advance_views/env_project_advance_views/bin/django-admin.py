3d029a93dc89284712c77dabaf06185e4065d52e
resita-alterra <resita@alterra.id>
2019-07-22 10:54:00 +0700

    push tugas django

---
+++ project_advance_views/env_project_advance_views/bin/django-admin.py
@@ -0,0 +1,5 @@
+#!/home/alta13/Documents/CLI/Hari1/alta3_13_resita/DJANGO_MVC/project_advance_views/env_project_advance_views/bin/python3
+from django.core import management
+
+if __name__ == "__main__":
+    management.execute_from_command_line()
---
+++ project_views/env_project_views/bin/django-admin.py
@@ -0,0 +1,5 @@
+#!/home/alta13/Documents/CLI/Hari1/alta3_13_resita/DJANGO_MVC/project_views/env_project_views/bin/python3
+from django.core import management
+
+if __name__ == "__main__":
+    management.execute_from_command_line()
